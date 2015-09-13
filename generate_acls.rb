0cd78082ca4e4f0021c3bd56acf56b8eb43fc7aa
Hogetaro <hogetaro@gmail.com>
2015-09-13 16:51:52 +0900

    acls now made from simple text

---
+++ generate_acls.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby
+
+require "./destinations.rb"
+
+TARGET="/usr/local/squid"
+
+Dir.glob("etc/acl-*").each do |file|
+  aclname = file.sub(/etc\/acl-[^-]+-(.*)\.txt/,'\1')
+  acltype = file.sub(/etc\/acl-([^-]+)-.*\.txt/,'\1')
+  open(file).each_line do |l|
+    l.gsub!(/\./,"\\.")
+    l.sub!(/$/,"$") if acltype =~ /dom/
+    l.sub!(/^/,"acl #{aclname} #{acltype} ")
+    puts l
+  end
+  puts "http_access deny #{aclname}"
+end
+
+
+
---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TARGET=/usr/local/squid
+
+# make conf
+rm -f build/*
+if [ ! -d build ]; then
+  mkdir build
+fi
+
+ruby generate_acls.rb > build/adblock-acls.conf
+
+# copy conf
+if [ ! -d $TARGET/etc/conf.d ]; then
+  sudo mkdir $TARGET/etc/conf.d
+fi
+
+# for some local one
+sudo cp etc/squid.conf $TARGET/etc
+sudo cp build/*.conf /usr/local/squid/etc/conf.d
+# for system one
+sudo cp build/*.conf /etc/squid/conf.d
+
+# restart squid
+sudo $TARGET/sbin/squid -f $TARGET/etc/squid.conf -k kill
+sudo $TARGET/sbin/squid -f $TARGET/etc/squid.conf
+
+if [ -f /etc/init.d/squid ]; then
+  sudo /etc/init.d/squid restart
+fi
+
+echo tail ...
+sudo tail -f $TARGET/var/logs/access.log |grep -v /403 |grep -v TCP_DENIED
+
+bash update-remote.sh
